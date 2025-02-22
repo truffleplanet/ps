9a06692d5c3a78869312f01602ce3d7ed157c40b
seunghun <kangseunghun9927@gmail.com>
2025-02-22 19:54:16 +0900

    [Silver IV] Title: 듣보잡, Time: 132 ms, Memory: 128324 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,22 +1,12 @@
 import sys
-from collections import defaultdict
 
 input_lines = sys.stdin.read().splitlines()
 
 N, M = map(int, input_lines[0].split())
-name_dict = defaultdict(lambda: 0)
 
-never_heard = input_lines[1: N+1]
-never_saw = input_lines[N+1:]
-gokhalnoreut = []
+never_heard = set(input_lines[1: N+1])
+never_saw = set(input_lines[N+1:])
 
-for name in never_heard:
-    name_dict[name] += 1
-
-for name in never_saw:
-    if name_dict[name]:
-        gokhalnoreut.append(name)
-
-gokhalnoreut.sort()
+gokhalnoreut = sorted(never_heard & never_saw)
 print(len(gokhalnoreut))
 print(*gokhalnoreut, sep="\n")
