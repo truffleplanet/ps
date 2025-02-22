ca0d7d06845a4228f1b38f6bbcdc35690900f1ba
seunghun <kangseunghun9927@gmail.com>
2025-02-22 18:24:28 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,29 +1,24 @@
 import sys
 
 input_data = sys.stdin.read().splitlines()
-h, w = map(int, input_data[0].split())
+N, M = map(int, input_data[0].split())
 trans_table = str.maketrans("WB", "01")
 
-board = [list(map(int, row.translate(trans_table))) for row in input_data[1:]]
+board = [int(row.translate(trans_table), base=2) for row in input_data[1:]]
 
-white = [0, 1, 0, 1, 0, 1, 0, 1]
-black = [1, 0, 1, 0, 1, 0, 1, 0]
-
-white_board = [white if i % 2 == 0 else black for i in range(8)]
-black_board = [black if i % 2 == 0 else white for i in range(8)]
+white_mask = [(i % 2) * 0b10101010 + ((i + 1) % 2) * 0b01010101 for i in range(8)]
+black_mask = [ x ^ 0b11111111 for x in white_mask]
 
 min_repaint = float('inf')
-
-for x in range(h - 8 + 1):
-    for y in range(w - 8 + 1):
+for x in range(N - 8 + 1):
+    for y in range(M - 8 + 1):
         repaint1 = 0
         repaint2 = 0
         for i in range(8):
-            for j in range(8):
-                if board[x + i][y + j] != white_board[i][j]:
-                    repaint1 += 1
-                if board[x + i][y + j] != black_board[i][j]:
-                    repaint2 += 1
+            board_slice = (board[x+i] >> (M - 8 - y) & 0b11111111)
+            repaint1 += bin(board_slice ^ white_mask[i]).count("1")
+            repaint2 += bin(board_slice ^ black_mask[i]).count("1")
+
         min_repaint = min(min_repaint, repaint1, repaint2)
 
 print(min_repaint)
