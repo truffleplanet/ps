9ce7bb6e9c1cd399652df0d1523c20394b342016
seunghun <kangseunghun9927@gmail.com>
2025-04-14 20:57:00 +0900

    [Silver III] Title: N과 M (6), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+
+path = []
+def backtrack(n):
+    if len(path) == M:
+        print(" ".join(map(str, path)))
+        return
+    
+    for i in range(n, N):
+            path.append(arr[i])
+            backtrack(i + 1)
+            path.pop()
+
+backtrack(0)
