05cc6354b38e4688660dce6b48abfd0cf61785cc
seunghun <kangseunghun9927@gmail.com>
2025-04-17 04:46:32 +0900

    [Silver II] Title: 종이의 개수, Time: 1196 ms, Memory: 167148 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -1,6 +1,5 @@
 import sys
 
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 N = int(input())
@@ -16,12 +15,19 @@ def seg(x, y, n):
             return (0, 0, 1)
     
     third = n // 3
-    subs = [
-        seg(x + dx * third, y + dy * third, third) 
-        for dy in range(3) 
-        for dx in range(3)
-    ]
-    a, b, c = [sum(x) for x in zip(*subs)]
+    s1 = seg(x, y, third)
+    s2 = seg(x + third, y, third)
+    s3 = seg(x + third * 2, y, third)
+    s4 = seg(x, y + third, third)
+    s5 = seg(x + third, y + third, third)
+    s6 = seg(x + third * 2, y + third, third)
+    s7 = seg(x, y + third * 2, third)
+    s8 = seg(x + third, y + third * 2, third)
+    s9 = seg(x + third * 2, y + third * 2, third)
+
+    a = s1[0] + s2[0] + s3[0] + s4[0] + s5[0] + s6[0] + s7[0] + s8[0] + s9[0] 
+    b = s1[1] + s2[1] + s3[1] + s4[1] + s5[1] + s6[1] + s7[1] + s8[1] + s9[1] 
+    c = s1[2] + s2[2] + s3[2] + s4[2] + s5[2] + s6[2] + s7[2] + s8[2] + s9[2]
 
     if (a, b, c).count(0) == 2:
         if a == 9:
@@ -37,4 +43,3 @@ ans = seg(0, 0, N)
 
 for a in ans:
     print(a)
-    
