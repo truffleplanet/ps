83c1a45e0c8fb47c55c350c86af3da4bcf116fcd
seunghun <kangseunghun9927@gmail.com>
2025-04-17 06:12:42 +0900

    [Bronze III] Title: 루트, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4619. 루트/루트.py
@@ -0,0 +1,13 @@
+import sys
+
+data = sys.stdin.read().splitlines()
+
+for line in data[:-1]:
+    b, n = map(int, line.split())
+    approx = round(b ** (1/n))
+    l = approx - 1
+    r = approx + 1
+    lst = [(abs(b - l ** n), l), (abs(b - approx ** n), approx), (abs(b - r ** n), approx)]
+    ans = min(lst)
+    print(ans[1])
+    
