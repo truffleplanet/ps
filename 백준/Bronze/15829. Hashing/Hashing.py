e6609ab5a45d827e83b1f43568be3723769d6054
seunghun <kangseunghun9927@gmail.com>
2025-04-19 04:01:03 +0900

    [Bronze II] Title: Hashing, Time: 92 ms, Memory: 108384 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,10 +1,13 @@
 r = 31
 M = 1234567901
 
-n = int(input())
+L = int(input())
 s = input()
 h = 0
-for i, c in enumerate(s):
-    h += (ord(c) - 96) * (r ** i)
-h %= M
+power = 1
+for i in range(L):
+    k = ord(s[i]) - 96
+    h = (h + (k * power)) % M
+    power = (power * r) % M
+
 print(h)
