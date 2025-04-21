d9b62505551ddcbe0d44ed63725da39ac089cb6c
seunghun <kangseunghun9927@gmail.com>
2025-04-22 00:22:28 +0900

    [Silver I] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,13 +1,13 @@
 def power(n, r, c):
-    half = r // 2
-
     if r == 0:
         return 1
 
+    half = power(n, r // 2, c)
+
     if r % 2 == 0:
-        return (power(n, half, c) ** 2) % c
+        return (half * half) % c
     else:
-        return ((power(n, half, c) ** 2) * n) % c
+        return (half * half * n) % c
 
 
 a, b, c = map(int, input().split())
