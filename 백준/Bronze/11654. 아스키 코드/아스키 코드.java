adf6f42426bc2a6f9a86c58cee8675f4a695e370
seunghun <kangseunghun9927@gmail.com>
2025-07-06 22:24:10 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char input = sc.next().charAt(0);
+        sc.close();
+        System.out.println((int) input);
+    }
+}
+    
