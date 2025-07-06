c9a3e5ea7ce1d9afc4b840e57e06c344b1b867ed
seunghun <kangseunghun9927@gmail.com>
2025-07-06 21:48:20 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]) {
+        int solved = 131;
+        String id = "kangseunghun9927";
+        
+        System.out.println(solved);
+        System.out.println(id);
+        
+    }
+}
