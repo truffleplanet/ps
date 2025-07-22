3dcd5742eff2095b5cbb0e26016b2c2908fee5f5
seunghun <kangseunghun9927@gmail.com>
2025-07-22 14:39:24 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 160 ms, Memory: 17768 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,18 +1,17 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		sc.close();
-		
-		for (int i = 0; i < N; i ++) {
-			StringBuilder sb = new StringBuilder();
-			for (int j = 0; j <= i; j++) {
-				sb.append("*");
+		int n = sc.nextInt();
+		StringBuilder output = new StringBuilder();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < i; j++) {
+				output.append("*");
 			}
-		System.out.println(sb.toString());
+			output.append("\n");
 		}
+		System.out.print(output);
 	}
 }
