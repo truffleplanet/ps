a6f7835e01029a01ecbf60306d2b3658c47a276c
seunghun <kangseunghun9927@gmail.com>
2025-08-08 16:49:48 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,382 ms, Memory: 27,372 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,39 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Solution {
 
-	static int N, ingrs[][], ans, Upper;
-	static boolean isSelected[];
-	static StringBuilder sb;
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
 			String[] tokens = br.readLine().split(" ");
-			N = Integer.parseInt(tokens[0]);
-			Upper = Integer.parseInt(tokens[1]);
-			ingrs = new int[N][2];
-			isSelected = new boolean[N];
+			int N = Integer.parseInt(tokens[0]);
+			int L = Integer.parseInt(tokens[1]);
+			int[][] ingrs = new int[N][2];
 
 			// 데이터 입력
 			for (int i = 0; i < N; i++) {
 				String[] ingr = br.readLine().split(" ");
-				ingrs[i][0] = Integer.parseInt(ingr[0]);
-				ingrs[i][1] = Integer.parseInt(ingr[1]);
+				ingrs[i][0] = Integer.parseInt(ingr[0]); // val
+				ingrs[i][1] = Integer.parseInt(ingr[1]); // cal
 			}
 
-			Arrays.sort(ingrs, (o1, o2) -> {
-				return o1[1] - o2[1];
-			});
+			int ans = -1;
 
-			ans = 0;
-			foodSelector(0, 0);
+			for (int i = 0; i < (1 << N); i++) {
+				int valSum = 0;
+				int calSum = 0;
+				for (int j = 0; j < N; j++) {
+					if ((i & (1 << j)) != 0) {
+						calSum += ingrs[j][1];
+						if (calSum > L) {
+							break;
+						}
+						valSum += ingrs[j][0];
+						ans = Math.max(ans, valSum);
+					}
+				}
+			}
+
 			sb.append("#").append(t).append(" ").append(ans).append("\n");
 		}
 
@@ -41,16 +46,4 @@ public class Solution {
 
 	}
 
-	public static void foodSelector(int value, int cals) {
-
-		ans = Math.max(ans, value);
-
-		for (int i = 0; i < N; i++) {
-			if (isSelected[i] || cals + ingrs[i][1] > Upper)
-				break;
-			isSelected[i] = true;
-			foodSelector(value + ingrs[i][0], cals + ingrs[i][1]);
-			isSelected[i] = false;
-		}
-	}
 }
