a36b0562f34fe7f92d5c955ae350fec7cc77930e
seunghun <kangseunghun9927@gmail.com>
2025-08-10 16:47:15 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 12008 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,10 +6,10 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class Main {
+	static final int[] dr = { 1, -1, 0, 0 };
+	static final int[] dc = { 0, 0, 1, -1 };
 
 	public static void main(String[] args) throws IOException {
-		final int[] dr = { 1, -1, 0, 0 };
-		final int[] dc = { 0, 0, 1, -1 };
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -16,8 +17,7 @@ public class Main {
 		int N = Integer.parseInt(tokens[0]);
 		int M = Integer.parseInt(tokens[1]);
 
-		boolean[][] map = new boolean[N][M]; // padding
-
+		boolean[][] map = new boolean[N][M];
 		for (int i = 0; i < N; i++) {
 			String line = br.readLine();
 			for (int j = 0; j < M; j++) {
@@ -27,56 +27,34 @@ public class Main {
 			}
 		}
 
-//		for (boolean[] maps : map) {
-//			System.out.println(Arrays.toString(maps));
-//		}
-
-		Point start = new Point(0, 0, 1);
-		boolean[][] visited = new boolean[N][M];
-		Queue<Point> queue = new ArrayDeque<>();
-		visited[start.r][start.c] = true;
-		queue.offer(start);
-		int ans = -1;
+		Queue<int[]> queue = new ArrayDeque<>();
+		int[][] dist = new int[N][M];
+		dist[0][0] = 1;
+		queue.offer(new int[] { 0, 0 });
 
 		while (!queue.isEmpty()) {
-			Point u = queue.poll();
-//			System.out.println(u);
-//			System.out.println(queue);
-			int r = u.r;
-			int c = u.c;
+			int[] cur = queue.poll();
+			int r = cur[0];
+			int c = cur[1];
 			if (r == N - 1 && c == M - 1) {
-				ans = u.depth;
 				break;
 			}
+
 			for (int k = 0; k < 4; k++) {
 				int nr = r + dr[k];
 				int nc = c + dc[k];
-				if (nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] && !visited[nr][nc]) {
-					visited[nr][nc] = true;
-					queue.offer(new Point(nr, nc, u.depth + 1));
-				}
+				if (nr < 0 || nr >= N || nc < 0 || nc >= M)
+					continue;
+				if (!map[nr][nc])
+					continue;
+				if (dist[nr][nc] != 0)
+					continue;
+				dist[nr][nc] = dist[r][c] + 1;
+				queue.offer(new int[] { nr, nc });
 			}
 		}
 
-		System.out.println(ans);
+		System.out.println(dist[N - 1][M - 1]);
+
 	}
-
-}
-
-class Point {
-	int r;
-	int c;
-	int depth;
-
-	public Point(int r, int c, int depth) {
-		this.r = r;
-		this.c = c;
-		this.depth = depth;
-	}
-
-	@Override
-	public String toString() {
-		return "Point [r=" + r + ", c=" + c + ", depth=" + depth + "]";
-	}
-
 }
