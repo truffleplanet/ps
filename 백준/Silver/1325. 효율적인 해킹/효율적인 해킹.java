337636439ac9d78d959f7ce2b9bec17bf35c6c9c
seunghun <kangseunghun9927@gmail.com>
2025-08-10 21:18:20 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10644 ms, Memory: 309200 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,76 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static List<List<Integer>> graph;
-    static boolean[] visited;
-    static int N, M;
-    static int[] result;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-        String[] tokens;
+		List<List<Integer>> adj = new ArrayList<>();
 
-        tokens = br.readLine().split(" ");
-        N = Integer.parseInt(tokens[0]);
-        M = Integer.parseInt(tokens[1]);
+		for (int i = 0; i <= N; i++) {
+			adj.add(new ArrayList<>());
+		}
 
-        graph = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
-        }
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			adj.get(v).add(u); // v - > u | u가 v를 신뢰한다.
+		}
 
-        for (int i = 0; i < M; i++) {
-            tokens = br.readLine().split(" ");
-            int u = Integer.parseInt(tokens[0]);
-            int v = Integer.parseInt(tokens[1]);
-            graph.get(v).add(u);
-        }
+		int best = 0;
+		List<Integer> candidates = new ArrayList<>();
+		for (int i = 1; i <= N; i++) {
+			int cnt = 1;
+			Queue<Integer> q = new ArrayDeque<>();
+			boolean[] visited = new boolean[N + 1];
+			q.offer(i);
+			visited[i] = true;
 
-        result = new int[N+1];
+			while (!q.isEmpty()) {
+				int u = q.poll();
+				for (int v : adj.get(u)) {
+					if (!visited[v]) {
+						visited[v] = true;
+						cnt++;
+						q.offer(v);
+					}
+				}
+			}
+			if (cnt > best) {
+				candidates = new ArrayList<>();
+				candidates.add(i);
+				best = cnt;
+			} else if (cnt == best) {
+				candidates.add(i);
+			} else {
+				continue;
+			}
+		}
 
-        for (int i = 1; i <= N; i++) {
-            visited = new boolean[N + 1];
-            dfs(i);
-        }
-
-        // 출력부
-        int max = 1;
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(max, result[i]);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            if (result[i] == max) {
-                sb.append(i).append(" ");
-            }
-        }
-
-        System.out.println(sb);
-
-    }
-
-    public static void dfs(int start) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        stack.push(start);
-        visited[start] = true;
-
-        while(!stack.isEmpty()) {
-            int cur = stack.pop();
-            for (int next : graph.get(cur)) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    result[start]++;
-                    stack.push(next);
-                }
-            }
-        }
-
-    }
+		for (int c : candidates) {
+			System.out.print(c + " ");
+		}
+	}
 
 }
