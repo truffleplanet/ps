a914520172034ce7f2a2482a06a2d3dad4d8f8b1
seunghun <kangseunghun9927@gmail.com>
2025-08-13 16:18:07 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 145 ms, Memory: 51,840 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -1,48 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Solution {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(br.readLine());
 
-		for (int t = 1; t <= T; t++) {
-			// 입력부
-			String[] line1 = br.readLine().split(" ");
-			int N = Integer.parseInt(line1[0]);
-			int B = Integer.parseInt(line1[1]);
-			String[] tokens = br.readLine().split(" ");
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+
+			st = new StringTokenizer(br.readLine());
 			int[] arr = new int[N];
 			for (int i = 0; i < N; i++) {
-				arr[i] = Integer.parseInt(tokens[i]);
+				arr[i] = Integer.parseInt(st.nextToken());
 			}
 
-			// 1. brute-force
-			int ans = Integer.MAX_VALUE;
-			int x = 1 << (N + 1) - 1;
-			for (int i = 0; i < x; i++) {
-				int sum = 0;
-				String indices = Integer.toBinaryString(i);
+			Arrays.sort(arr);
 
-				// 뒤부터 읽을 것이고,
-				// n자릿수 값이 1이면 arr[n-1]이 포함된 집합임.
-				for (int k = indices.length() - 1, idx = 0; k >= 0; k--, idx++) {
-					if (indices.charAt(k) == '1') {
-						sum += arr[idx];
+			// 여러 합 조합을 만들어보는데,
+			// B보다 크면 그 분기는 멈추고
+			// 그 외에 모든 경우가 빠짐없이
+			int ans = Integer.MAX_VALUE;
+
+			for (int i = 0; i < N; i++) {
+				Deque<int[]> stack = new ArrayDeque<>();
+				if (arr[i] >= B) {
+					ans = Math.min(ans, arr[i]);
+					break;
+				}
+				stack.push(new int[] { i, arr[i] });
+				while (!stack.isEmpty()) {
+					int[] node = stack.pollLast();
+					int cur = node[0];
+					int sum = node[1];
+
+					for (int k = cur + 1; k < N; k++) {
+						if (sum + arr[k] >= B) {
+							ans = Math.min(ans, sum + arr[k]);
+							break;
+						}
+						stack.offerLast(new int[] { k, sum + arr[k] });
 					}
 				}
-				if (sum >= B) {
-					ans = Math.min(ans, sum);
-				}
 			}
 
-            ans = ans - B;
-			sb.append("#" + t + " " + ans).append("\n");
-
+			ans -= B;
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
 		}
 
 		System.out.println(sb);
