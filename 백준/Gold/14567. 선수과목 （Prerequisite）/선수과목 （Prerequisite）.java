36b99d34e109cfc5d0d9616041d0ff275b6939d4
seunghun <kangseunghun9927@gmail.com>
2025-08-30 19:16:00 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 512 ms, Memory: 131480 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -1,8 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 /*
@@ -45,14 +47,34 @@ import java.util.StringTokenizer;
  * 	들어오는 간선이 없는 노드들 전부 기록, 해당 노드들에서 나가는 간선 전부 제거 
  * 	학기 기록
  * 
- * 디테일:
+ * 구현 1:
  * 위상 정렬에 자신감이 없네.
  * 일단 아이디어에 맞게, 들어오는 간선을 노드별로 세어두고
  * 매번 전체 노드 순회하면서 하는 brute force 구현 해보자. 
  * 
+ * 
+ * 구현 2:
+ * bfs
+ * 1. 들어오는 간선이 0인 노드를 모두 큐에 넣는다. 
+ * 2. 해당 노드들에서 나가는 간선을 지우면서, 지웠을 때 들어오는 간선이 0이되는 노드들이 있는지 확인한다.
+ * 3. 해당 노드들은 큐에 삽입한다.
+ * - 현재 문제에서는 level이 필요함으로, 노드는 그냥 구현해서 쓰기로 한다. 
+ * 
  */
 
 public class Main {
+
+	static class Node {
+		int id;
+		int level;
+
+		private Node(int id, int level) {
+			super();
+			this.id = id;
+			this.level = level;
+		}
+
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -63,45 +85,90 @@ public class Main {
 		int V = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
 
-		int[] count = new int[V + 1];
+		// 1st try
+//		int[] count = new int[V + 1];
+//		List<List<Integer>> G = new ArrayList<>();
+//		for (int i = 0; i < V + 1; i++) {
+//			G.add(new ArrayList<Integer>());
+//		}
+//
+//		for (int i = 0; i < E; i++) {
+//			st = new StringTokenizer(br.readLine());
+//			int u = Integer.parseInt(st.nextToken());
+//			int v = Integer.parseInt(st.nextToken());
+//			count[v]++;
+//			G.get(u).add(v);
+//		}
+//
+//		int[] ans = new int[V + 1];
+//		int ith = 1;
+//		int nodeCnt = 0;
+//		List<Integer> curr = new ArrayList<>();
+//		while (nodeCnt < V) {
+//			curr.clear();
+//			for (int i = 1; i < V + 1; i++) {
+//				if (count[i] == 0) {
+//					count[i] = -1;
+//					nodeCnt++;
+//					ans[i] = ith;
+//					curr.add(i);
+//				}
+//			}
+//
+//			for (int u : curr) {
+//				for (int v : G.get(u)) {
+//					count[v]--;
+//				}
+//			}
+//
+//			ith++;
+//		}
+//
+//		for (int i = 1; i < V + 1; i++) {
+//			sb.append(ans[i]).append(" ");
+//		}
+//
+//		System.out.println(sb);
+
+		// 2nd try
+		int[] inCount = new int[V + 1]; // 들어오는 간선의 수를 기록할 배열
+		int[] ans = new int[V + 1]; // 정답을 기록할 배열
 		List<List<Integer>> G = new ArrayList<>();
-		for (int i = 0; i < V + 1; i++) {
-			G.add(new ArrayList<Integer>());
-		}
+		Queue<Node> q = new ArrayDeque<>();
+
+		// init Graph
+		for (int i = 0; i <= V; i++)
+			G.add(new ArrayList<>());
 
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
-			count[v]++;
+			inCount[v]++;
 			G.get(u).add(v);
 		}
 
-		int[] ans = new int[V + 1];
-		int ith = 1;
-		int nodeCnt = 0;
-		List<Integer> curr = new ArrayList<>();
-		while (nodeCnt < V) {
-			curr.clear();
-			for (int i = 1; i < V + 1; i++) {
-				if (count[i] == 0) {
-					count[i] = -1;
-					nodeCnt++;
-					ans[i] = ith;
-					curr.add(i);
-				}
+		// 시작 정점 삽입
+		for (int i = 1; i <= V; i++) {
+			if (inCount[i] == 0) {
+				q.offer(new Node(i, 1)); // id, level;
+				ans[i] = 1;
 			}
-
-			for (int u : curr) {
-				for (int v : G.get(u)) {
-					count[v]--;
-				}
-			}
-
-			ith++;
 		}
 
-		for (int i = 1; i < V + 1; i++) {
+		while (!q.isEmpty()) {
+			Node u = q.poll();
+			for (int v : G.get(u.id)) {
+				inCount[v]--;
+				if (inCount[v] == 0) {
+					q.offer(new Node(v, u.level + 1));
+					// 다음에 방문하려고 하면 inCount[v] == -1이 되어서 불가능하니 걱정말자
+					ans[v] = u.level + 1;
+				}
+			}
+		}
+
+		for (int i = 1; i <= V; i++) {
 			sb.append(ans[i]).append(" ");
 		}
 
