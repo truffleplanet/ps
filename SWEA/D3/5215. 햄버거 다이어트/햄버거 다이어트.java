a13945f2b3b2194d8271cecc3f90978b349050b7
seunghun <kangseunghun9927@gmail.com>
2025-09-03 21:54:00 +0900

    [D3] Title: 햄버거 다이어트, Time: 123 ms, Memory: 37,476 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -3,90 +3,60 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+ * 0/1 knapsack
+ * 
+ * 
+ */
+
 public class Solution {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
+
 		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
-
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int L = Integer.parseInt(st.nextToken());
 
-			int[][] stuffs = new int[N][2];
+			int[][] vec = new int[N][2]; // {val, cal}
 
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				int val = Integer.parseInt(st.nextToken());
 				int cal = Integer.parseInt(st.nextToken());
-				stuffs[i][0] = val;
-				stuffs[i][1] = cal;
+				vec[i][0] = val;
+				vec[i][1] = cal;
 			}
 
-			int max = 0;
-			int[] p = new int[N];
-			for (int i = N - 1; i >= 0; i--) {
-				p[i] = 1;
-				int[] tp = p.clone();
-				do {
-					int valSum = 0;
-					int calSum = 0;
-					for (int k = 0; k < N; k++) {
-						if (tp[k] == 0)
-							continue;
-						if (calSum + stuffs[k][1] > L) {
-							break;
-						}
-						valSum += stuffs[k][0];
-						calSum += stuffs[k][1];
+			int[][] dp = new int[N][L + 1];
+			int val0 = vec[0][0];
+			int cal0 = vec[0][1];
+			for (int j = cal0; j <= L; j++) {
+				dp[0][j] = val0;
+			}
+
+			for (int i = 1; i < N; i++) {
+				int val = vec[i][0];
+				int cal = vec[i][1];
+				for (int j = 0; j <= L; j++) {
+					if (j >= cal) {
+						dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cal] + val);
+					} else {
+						dp[i][j] = dp[i - 1][j];
 					}
-					max = Math.max(max, valSum);
-				} while (np(tp));
-			}
 
-			sb.append("#").append(tc).append(" ").append(max).append("\n");
+				}
+			}
+			sb.append("#").append(tc).append(" ").append(dp[N - 1][L]).append("\n");
 
 		}
 
 		System.out.println(sb);
-
-	}
-
-	public static boolean np(int[] a) {
-		int N = a.length;
-
-		int i = N - 1;
-		while (i > 0 && a[i - 1] >= a[i])
-			i--;
-
-		if (i == 0)
-			return false;
-
-		int j = N - 1;
-		while (a[i - 1] >= a[j])
-			j--;
-
-		swap(a, i - 1, j);
-
-		int l = i;
-		int r = N - 1;
-		while (l < r)
-			swap(a, l++, r--);
-
-		return true;
-	}
-
-	public static void swap(int[] a, int i, int j) {
-		if (i == j)
-			return;
-		int tmp = a[i];
-		a[i] = a[j];
-		a[j] = tmp;
-
 	}
 
 }
