83e476ad8ebf63fe9093f2dedb8985e7c233724e
seunghun <kangseunghun9927@gmail.com>
2025-09-04 17:14:34 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 1084 ms, Memory: 95812 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -40,14 +39,13 @@ public class Main {
 			G[u].add(new int[] { v, w });
 		}
 
-		int[][] dist = new int[V + 1][K + 1];
-
+		PriorityQueue<Integer>[] dist = new PriorityQueue[V + 1];
 		for (int i = 1; i <= V; i++) {
-			Arrays.fill(dist[i], INF);
+			dist[i] = new PriorityQueue<>(Comparator.reverseOrder());
 		}
 
 		PriorityQueue<int[]> pq = new PriorityQueue<int[]>(Comparator.comparingInt(n -> n[1]));
-		dist[1][1] = 0;
+		dist[1].offer(0);
 		pq.offer(new int[] { 1, 0 });
 
 		while (!pq.isEmpty()) {
@@ -55,43 +53,30 @@ public class Main {
 			int uId = u[0];
 			int ud = u[1];
 
-			boolean match = false;
-			for (int k = 1; k <= K; k++) {
-				if (ud == dist[uId][k]) {
-					match = true;
-					break;
-				}
-			}
-
-			if (!match)// stale
-				continue;
-
 			for (int[] e : G[uId]) {
 				int vId = e[0];
-				int w = e[1];
+				int nd = ud + e[1];
 
-				int nd = ud + w;
-
-				if (nd < dist[vId][K]) {
-					dist[vId][K] = nd;
-					Arrays.sort(dist[vId], 1, K + 1);
+				if (dist[vId].size() < K) {
+					dist[vId].offer(nd);
+					pq.offer(new int[] { vId, nd });
+				} else if (dist[vId].peek() > nd) {
+					dist[vId].poll();
+					dist[vId].offer(nd);
 					pq.offer(new int[] { vId, nd });
 				}
 			}
 		}
 
 		for (int i = 1; i <= V; i++) {
-			int kth = dist[i][K];
-			if (kth == INF) {
+			if (dist[i].size() < K)
 				sb.append(-1);
-			} else {
-				sb.append(kth);
-			}
+			else
+				sb.append(dist[i].peek());
+
 			sb.append("\n");
 		}
 
 		System.out.println(sb);
-
 	}
-
 }
