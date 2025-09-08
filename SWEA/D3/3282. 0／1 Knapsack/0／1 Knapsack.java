d8b18dff5eb4d54bd1c47d5fd9e8863636d7ab83
seunghun <kangseunghun9927@gmail.com>
2025-09-08 11:56:10 +0900

    [D3] Title: 0/1 Knapsack, Time: 112 ms, Memory: 28,684 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -17,36 +17,31 @@ public class Solution {
 			int N = Integer.parseInt(st.nextToken());
 			int Cap = Integer.parseInt(st.nextToken());
 
-			int[][] stuffs = new int[N][2];
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				stuffs[i][0] = Integer.parseInt(st.nextToken());
-				stuffs[i][1] = Integer.parseInt(st.nextToken());
-			}
-
 			int[][] oRow = new int[2][Cap + 1];
-
 			int prev = 0;
 
-			for (int s = 0; s < N; s++) {
-				int w = stuffs[s][0];
-				int v = stuffs[s][1];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				int w = Integer.parseInt(st.nextToken());
+				int v = Integer.parseInt(st.nextToken());
 				int next = prev ^ 1;
+
 				for (int c = 0; c <= Cap; c++) {
+
 					if (c < w) {
 						oRow[next][c] = oRow[prev][c];
 						continue;
 					}
+
 					oRow[next][c] = Math.max(oRow[prev][c], v + oRow[prev][c - w]);
+
 				}
-//				for (int i = 0; i <= 1; i++) {
-//					System.out.println(Arrays.toString(oRow[i]));
-//				}
+
 				prev ^= 1;
+
 			}
 
 			sb.append('#').append(tc).append(' ').append(oRow[prev][Cap]).append('\n');
-
 		}
 
 		System.out.println(sb);
