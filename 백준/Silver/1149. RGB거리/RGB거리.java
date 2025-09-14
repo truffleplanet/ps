5c68caa8615a96f0c89c198135710acd9bda8308
seunghun <kangseunghun9927@gmail.com>
2025-09-15 01:11:35 +0900

    [Silver I] Title: RGB거리, Time: 76 ms, Memory: 11944 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,35 +11,30 @@ public class Main {
 
 		int N = Integer.parseInt(br.readLine());
 
-		int[][] dp = new int[N][3];
-
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(dp[i], Integer.MAX_VALUE);
-		}
+		int[] prev = new int[3];
 
 		st = new StringTokenizer(br.readLine());
-		dp[0][0] = Integer.parseInt(st.nextToken());
-		dp[0][1] = Integer.parseInt(st.nextToken());
-		dp[0][2] = Integer.parseInt(st.nextToken());
+		prev[0] = Integer.parseInt(st.nextToken());
+		prev[1] = Integer.parseInt(st.nextToken());
+		prev[2] = Integer.parseInt(st.nextToken());
 
-		int[] buffer = new int[3];
+		int[] cur = new int[3];
 		for (int i = 1; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int d = 0; d < 3; d++) {
-				buffer[d] = Integer.parseInt(st.nextToken());
-			}
+			int r = Integer.parseInt(st.nextToken());
+			int g = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 
-			for (int d = 0; d < 3; d++) {
-				for (int k = 0; k < 3; k++) {
-					if (k == d)
-						continue;
-					dp[i][d] = Math.min(buffer[d] + dp[i - 1][k], dp[i][d]);
-				}
-			}
+			cur[0] = r + Math.min(prev[1], prev[2]);
+			cur[1] = g + Math.min(prev[0], prev[2]);
+			cur[2] = b + Math.min(prev[0], prev[1]);
 
+			int[] tmp = prev;
+			prev = cur;
+			cur = tmp;
 		}
 
-		int ans = Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2]));
+		int ans = Math.min(prev[0], Math.min(prev[1], prev[2]));
 		System.out.println(ans);
 
 	}
