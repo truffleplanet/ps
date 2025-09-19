96db263ce63b86a721e6f5c1104e914768ffb1a2
seunghun <kangseunghun9927@gmail.com>
2025-09-19 15:56:59 +0900

    [D3] Title: 0/1 Knapsack, Time: 112 ms, Memory: 28,684 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -1,50 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-
-		int T = Integer.parseInt(br.readLine());
-
-		for (int tc = 1; tc <= T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int Cap = Integer.parseInt(st.nextToken());
-
-			int[][] oRow = new int[2][Cap + 1];
-			int prev = 0;
-
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				int w = Integer.parseInt(st.nextToken());
-				int v = Integer.parseInt(st.nextToken());
-				int next = prev ^ 1;
-
-				for (int c = 0; c <= Cap; c++) {
-
-					if (c < w) {
-						oRow[next][c] = oRow[prev][c];
-						continue;
-					}
-
-					oRow[next][c] = Math.max(oRow[prev][c], v + oRow[prev][c - w]);
-
-				}
-
-				prev ^= 1;
-
-			}
-
-			sb.append('#').append(tc).append(' ').append(oRow[prev][Cap]).append('\n');
-		}
-
-		System.out.println(sb);
-	}
-
+    public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st;
+      StringBuilder sb = new StringBuilder();
+      
+      int T = Integer.parseInt(br.readLine());
+      
+      for (int tc = 1; tc <= T; tc++) {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[][] dp = new int[2][K+1];
+        int cur = 0;
+        
+        for (int i = 0; i < N; i++) {
+          int next = cur ^ 1;
+          st = new StringTokenizer(br.readLine());
+          int v = Integer.parseInt(st.nextToken());
+          int c = Integer.parseInt(st.nextToken());
+          
+          for (int k = 0; k <= K; k++) {
+            if (k < v) {
+              dp[next][k] = dp[cur][k];
+            } else {
+              dp[next][k] = Math.max(dp[cur][k-v] + c, dp[cur][k]);
+            }
+          }
+          cur = next;
+        }
+        
+        int ans = Math.max(dp[0][K], dp[1][K]);
+        
+        sb.append('#').append(tc).append(' ').append(ans).append('\n');
+        
+      }
+      
+      System.out.println(sb);
+  }
 }
