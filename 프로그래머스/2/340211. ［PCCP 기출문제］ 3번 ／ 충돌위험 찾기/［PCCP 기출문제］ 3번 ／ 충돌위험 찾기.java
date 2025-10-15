148d6f2a2e9f5daa1931f91a474ad3756e919dfd
seunghun <kangseunghun9927@gmail.com>
2025-10-15 16:11:35 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 410.34 ms, Memory: 166 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] points, int[][] routes) {
+        Map<Integer, Integer> stamp = new HashMap<>();
+        
+        for (int[] route : routes) {
+            int r = points[route[0] - 1][0];
+            int c = points[route[0] - 1][1];
+            int t = 0;
+            int key = getKey(r, c, t);
+            stamp.put(key, stamp.getOrDefault(key, 0) + 1);
+
+            for (int i = 1; i < route.length; i++) {
+                int nr = points[route[i] - 1][0];
+                int nc = points[route[i] - 1][1];
+
+                int d = (r > nr) ? -1 : 1;
+                while(r != nr) {
+                    r += d;
+                    t++;
+                    key = getKey(r, c, t);
+                    stamp.put(key, stamp.getOrDefault(key, 0) + 1);
+                }
+                
+                d = (c > nc) ? -1 : 1;
+                while (c != nc) {
+                    c += d;
+                    t++;
+                    key = getKey(r, c, t);
+                    stamp.put(key, stamp.getOrDefault(key, 0) + 1);
+                }
+            }
+        }
+        
+        int answer = 0;
+        
+        for (int val : stamp.values()) {
+            if (val >= 2) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    int getKey(int r, int c, int t) {
+        return (r << 25) + (c << 17) + t;
+    }
+    
+}
