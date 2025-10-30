b325928d8b69350f6c15ef5f1300edf09c2215c1
seunghun <kangseunghun9927@gmail.com>
2025-10-31 00:25:02 +0900

    [Silver III] Title: 1로 만들기, Time: 68 ms, Memory: 12556 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,74 +1,72 @@
 /*
- * 
- */
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.Scanner;
+예시: 10의 경우에 10 → 9 → 3 → 1 로 3번 만에 만들 수 있다.
+예시에서 알 수 있듯이 나눌 수 있다고 나누는 그리디 접근은 정답을 보장하지 않는다.
 
-public class Main {
+따라서 boolean[] visited만들고
+bfs로 서치한다.
 
-	static class Node {
-		int u;
-		int d;
+상태는 최대 10^6 즉 백만이므로
+1억번 연산을 1초에 한다고 가정하면
+0.01초에 근사한 시간에 풀 수 있을 것이다.
+*/
 
-		public Node(int u, int d) {
-			super();
-			this.u = u;
-			this.d = d;
-		}
 
-	}
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		sc.close();
+// The main method must be in a class named "Main".
+class Main {
 
-		int cnt = 0;
+    static class State {
+        int val;
+        int dist;
 
-		int[] dist = new int[N + 1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
+        public State(int val, int dist) {
+            this.val = val;
+            this.dist = dist;
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-		Queue<Node> q = new ArrayDeque<>();
-		q.offer(new Node(N, 0));
-		dist[N] = 0;
+        if (N == 1) {
+            System.out.println(0);
+            return;
+        }
+        
+        boolean[] visited = new boolean[N];
+        Queue<State> q = new ArrayDeque<>();
+        q.offer(new State(N, 0));
+        
+        while (!q.isEmpty()) {
+            State state = q.poll();
+            int cur = state.val;
+            int nd = state.dist + 1;
+            
+            int v1 = cur - 1;
+            int v2 = (cur % 2 == 0) ? (cur / 2) : v1;
+            int v3 = (cur % 3 == 0) ? (cur / 3) : v1;
 
-		while (!q.isEmpty()) {
-			Node node = q.poll();
-			int u = node.u;
-			int nd = node.d + 1;
+            int[] vals = new int[] {v1, v2, v3};
 
-			if (u == 1) {
-				System.out.println(node.d);
-				return;
-			}
+            for (int v : vals) {
+                if (v == 1) {
+                    System.out.println(nd);
+                    return;
+                }
+                
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.offer(new State(v, nd));
+                }
+                
+            }
 
-			int v1 = u - 1;
-			if (dist[v1] > nd) {
-				dist[v1] = nd;
-				q.offer(new Node(v1, nd));
-			}
-
-			if (u % 2 == 0) {
-				int v2 = u / 2;
-				if (dist[v2] > nd) {
-					dist[v2] = nd;
-					q.offer(new Node(v2, nd));
-				}
-			}
-
-			if (u % 3 == 0) {
-				int v3 = u / 3;
-				if (dist[v3] > nd) {
-					dist[v3] = nd;
-					q.offer(new Node(v3, nd));
-				}
-			}
-
-		}
-
-	}
-
+        }
+        
+    }
 }
