e812a5de64ae818c5705a4f2eb9e4ee41bdf6589
seunghun <kangseunghun9927@gmail.com>
2025-12-18 18:54:23 +0900

    [D3] Title: 0/1 Knapsack, Time: 87 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -8,14 +8,13 @@ class Solution {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        final int T = Integer.parseInt(br.readLine());
-        int[] dp = new int[1001];
+        int T = Integer.parseInt(br.readLine());
         
         for (int tc = 1; tc <= T; tc++) {
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
-            Arrays.fill(dp, 0, K + 1, 0);
+        	int[] dp = new int[K + 1];
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
