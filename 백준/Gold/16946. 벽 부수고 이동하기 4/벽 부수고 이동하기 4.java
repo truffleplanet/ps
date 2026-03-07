405f59e7214d6bd6804377f4d0894f6231df3e18
seunghun <kangseunghun9927@gmail.com>
2026-03-07 12:35:06 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 464 ms, Memory: 67180 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -8,8 +8,11 @@ class Main {
     final static int[] DR = {1, 0, -1, 0};
     final static int[] DC = {0, 1, 0, -1};
 
-    static int H;
-    static int W;
+    static int H, W;
+    static boolean[][] isWall;
+    static int[][] groupIdMap;
+    static List<Integer> groupSize;
+    static int[] q;
     
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,101 +20,109 @@ class Main {
 
         H = Integer.parseInt(st.nextToken());
         W = Integer.parseInt(st.nextToken());
-        boolean[][] isWall = new boolean[H][W];
-        int[][] getGroupId = new int[H][W];
-        List<Integer> groupCnt = new ArrayList<>();
-        groupCnt.add(-1);
+        
+        isWall = new boolean[H][W];
+        groupIdMap = new int[H][W];
+        q = new int[H * W];
+        groupSize = new ArrayList<>();
+        groupSize.add(-1);
         
         for (int i = 0; i < H; i++) {
             String s = br.readLine();
             for (int j = 0; j < W; j++) {
-                isWall[i][j] = (s.charAt(j) == '1') ? true : false; 
+                isWall[i][j] = (s.charAt(j) == '1');
             }
         }
 
         // 빈 공간 군집화 계산
-        int AUTO_INCREMENT_ID = 1;
-        Queue<Integer> q = new ArrayDeque<>();
+        int currentGroupId = 1;
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < W; j++) {
-                if (isWall[i][j] || getGroupId[i][j] != 0) {
+                if (isWall[i][j] || groupIdMap[i][j] != 0) {
                     continue;
                 }
-                getGroupId[i][j] = AUTO_INCREMENT_ID++;
-                q.offer(getState(i, j));
-                int cnt = 1;
-                while (!q.isEmpty()) {
-                    int state = q.poll();
-                    int r = getR(state);
-                    int c = getC(state);
-
-                    for (int d = 0; d < 4; d++) {
-                        int nr = r + DR[d];
-                        int nc = c + DC[d];
-
-                        if (nr < 0 || nr >= H || nc < 0 || nc >= W) {
-                            continue;
-                        }
-                        if (isWall[nr][nc]) {
-                            continue;
-                        }
-                        if (getGroupId[nr][nc] != 0) {
-                            continue;
-                        }
-
-                        getGroupId[nr][nc] = getGroupId[i][j];
-                        cnt++;
-                        q.offer(getState(nr, nc));
-                    }
-                }
-                groupCnt.add(cnt % 10);
+                int size = bfs(i, j, currentGroupId);
+                groupSize.add(size % 10);
+                currentGroupId++;
             }
         }
 
-        q.clear();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < W; j++) {
                 if (!isWall[i][j]) {
                     sb.append('0');
-                    continue;
+                } else {
+                    sb.append(countMovable(i, j) % 10);
                 }
-                int cnt = 1;
-                HashSet<Integer> visited = new HashSet<>();
-                visited.add(0);
-                for (int d = 0; d < 4; d++) {
-                    int nr = i + DR[d];
-                    int nc = j + DC[d];
-                    
-                    if (nr < 0 || nr >= H || nc < 0 || nc >= W) {
-                        continue;
-                    }
-                    
-                    int groupId = getGroupId[nr][nc];
-                    if (visited.contains(groupId)) {
-                        continue;
-                    }
-                    visited.add(groupId);
-                    cnt += groupCnt.get(groupId);
-                }
-                cnt %= 10;
-                sb.append(cnt);
             }
-            
             sb.append('\n');
         }
         System.out.println(sb);
     }
 
-    private static int getState(int r, int c) {
-        return r * W + c;
+    private static int bfs(int startR, int startC, int id) {
+        int head = 0;
+        int tail = 0;
+        
+        q[tail++] = (startR << 10) | startC;
+        groupIdMap[startR][startC] = id;
+        
+        int cnt = 1;
+        while (head < tail) {
+            int state = q[head++];
+            int r = state >> 10;
+            int c = state & 1023;
+
+            for (int d = 0; d < 4; d++) {
+                int nr = r + DR[d];
+                int nc = c + DC[d];
+
+                if (nr < 0 || nr >= H || nc < 0 || nc >= W) {
+                    continue;
+                }
+                if (isWall[nr][nc] || groupIdMap[nr][nc] != 0) {
+                    continue;
+                }
+                groupIdMap[nr][nc] = id;
+                cnt++;
+                q[tail++] = (nr << 10) | nc;
+            }
+        }
+        return cnt;
     }
 
-    private static int getR(int state) {
-        return state / W;
-    }
-    
-    private static int getC(int state) {
-        return state % W;
+    private static int countMovable(int r, int c) {
+        int total = 1;
+        int[] seenIds = new int[4];
+        int seenCount = 0;
+
+        for (int d = 0; d < 4; d++) {
+            int nr = r + DR[d];
+            int nc = c + DC[d];
+
+            if (nr < 0 || nr >= H || nc < 0 || nc >= W) {
+                continue;
+            }
+
+            if (isWall[nr][nc]) {
+                continue;
+            }
+
+            int groupId = groupIdMap[nr][nc];
+            boolean isDuplicate = false;
+            for (int i = 0; i < seenCount; i++) {
+                if (seenIds[i] == groupId) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (!isDuplicate) {
+                seenIds[seenCount++] = groupId;
+                total += groupSize.get(groupId);
+            }
+        }
+        return total;
     }
 }
