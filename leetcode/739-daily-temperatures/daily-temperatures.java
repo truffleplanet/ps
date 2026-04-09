18d1c3a1c9d766c788de021371cd1e15b62c536d
SeunghunKang <kangseunghun9927@gmail.com>
2026-04-09 15:28:13 +0900

    Time: 26 ms (81.82%) | Memory: 103.6 MB (87.95%) - LeetSync

---
+++ leetcode/739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,36 @@
+/*
+brute force O(N^2)
+매 위치별로 다음 위치 찾기 -> 10^10
+---
+
+찾아야 할 것 -> 나보다 따뜻한 날
+temperatures의 idx는 시점이므로, 
+삽입된 시점을 순차적으로 stack에 넣는다.
+    - 매 삽입 시 자신보다 큰 값이 나올 때까지 pop 하며 삽입하려는 idx - idx_pop이 시간이 된다.
+*/
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] out = new int[temperatures.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < temperatures.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(i);
+            } else {
+                while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                    int past = stack.pop();
+                    out[past] = i - past;
+                }
+                stack.push(i);
+            }
+        }
+        
+        while (!stack.isEmpty()) {
+            int past = stack.pop();
+            out[past] = 0;
+        }
+
+        return out;
+    }
+}
